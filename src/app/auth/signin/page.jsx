f5e113157084ad6f126c54500f8b494196ff5eda
deepanrvkm-at-905418272189 <dcubeinnovations.@gmail.com>
2024-08-07 23:30:20 +0530

    Build error

--- src/app/auth/signin/page.jsx
+++ src/app/auth/signin/page.jsx
@@ -84,7 +84,7 @@ export default function SignIn() {
         <SubmitButton>Sign In</SubmitButton>
       </form>
       <p className="mt-4 text-center">
-        Don't have an account? <Link href="/auth/register" className="text-[#00b4b9]">Register here</Link>
+        Don&apos;t have an account? <Link href="/auth/register" className="text-[#00b4b9]">Register here</Link>
       </p>
     </AuthLayout>
   );
